b678a384c2a3e3dcfabd55d5e30ebf46ba4f39eb
drobinson6045 <drobinson6045@live.com>
2019-03-02 16:33:14 -0500

    potential with square geometry only

--- potential.m
+++ potential.m
@@ -3,12 +3,12 @@ global M N u0 uR totalPts nPnts nMiss;
 
 
 
-M = 50;
+M = 10;
 
-N = 50;
-u0 = 2.0;
-domX = [0 100.0];
-domY = [0 100.0];
+N = 10;
+u0 = 1.0;
+domX = [0 1.0];
+domY = [0 1.0];
 dx = (domX(2)-domX(1))/(N-1);
 dy = (domY(2)-domY(1))/(N-1);
 xGrid = linspace(domX(1),domX(2),N);
@@ -21,18 +21,19 @@ cells(M+1,:)=0.0;
 
 
 %Initialization
-nSolid = 4;%this is a bit sloppy (Has to match with filling of solid cells
+nSolid = 0;%this is a bit sloppy (Has to match with filling of solid cells
     %Fill solid cells for outflow boundary
-cells(M-1:M,N-1:N)=0.0;
+%cells(M-1:M,N-1:N)=0.0;
 
-totalPts = M*N-nSolid;
+totalPts = (M-2)*(N-2)-nSolid;
 rhs = zeros(totalPts);
 A = zeros(totalPts,totalPts);
 b = zeros(totalPts,1);
 x0 = zeros(totalPts,1);
-nPnts = M*ones(M,1);
+nPnts = zeros(M,1);
 nMiss = zeros(M,1);
-%fill solid cells
+
+%fill solid cells to block outflow
 for j = 1:M+1
     if(cells(j,N)==0)
         cells(j,N+1)=0.0;
@@ -42,26 +43,26 @@ end
 
     %account for non square geometry
 total=0;
-for j=1:M
+for j=2:M-1
     count=0;
-    for k=1:N
-        if(sum(abs(cells(j:j+1,k))+abs(cells(j:j+1,k+1)))~=0)
+    for k=2:N-1
+        if(sum(abs(cells(j:j+1,k))+abs(cells(j:j+1,k+1)))==4)
             total=total+1;
             count=count+1;
         end
     end
     nPnts(j) = total;
-    nMiss(j)= M-count;
+    nMiss(j)= M-2-count;
 end
 
 
 %count missing right cells
-count=0;
-for i=1:M
-    if(nPnts(i)-i*N == 0)
-        count=count + 1;
-    end
-end
+% count=0;
+% for i=1:M
+%     if(nPnts(i)-i*N == 0)
+%         count=count + 1;
+%     end
+% end
 
 uR = u0*(M-1)/(count-1);%conservation motivated
     
@@ -74,19 +75,34 @@ for id=1:totalPts
 end
 disp("built matrix")
 %build initial guess of uniform flow
-for i=1:M
+for i=2:M-1
    if(i==1)
        for j=1:nPnts(i)
-           x0(j)=xGrid(j);
+           x0(j)=xGrid(j+1);
        end
    else
        for j=1:nPnts(i)-nPnts(i-1)
-           x0(nPnts(i-1)+j)=xGrid(j);
+           x0(nPnts(i-1)+j)=xGrid(j+1);
        end
    end
 end
 
 x = SOR(A,b,x0,1E-3);
+
+solution = rebuildGrid(x,nPnts);
+[vx,vy] = detVel(solution,dx);
+
+%draw cells
+drawCells(domX,domY,dx,cells);
+
+hold on
+%draw vel field
+[xm, ym] = meshgrid(xGrid(2:N-1),yGrid(2:M-1));
+size(xm)
+quiver(xm,ym,vx,vy);
+
+
+
 
 function X = SOR( A ,B, X0 ,t)
 [na , ma ] = size (A);
@@ -108,15 +124,15 @@ e= max(eig(( D+w*L) \ ( D*(1-w) - w*U)))
  
 if abs(e) >= 1
     disp ('largest eigen value of iterative matrix is not less than 1') 
-    disp ('not convergent. Please try some other process.')
+    disp ('not convergent. ')
     disp(e)
-    return
+    %return
 end
  
 
 [nx, mx] = size(X0);
 if nx ~= na || mx ~= 1
-    disp( 'ERROR: pls check input')
+    disp( 'ERROR: check input')
     return
 end
 
@@ -127,12 +143,11 @@ end
 tol = t*ones(na,1);
 k= 1;
  
-X( : , 1 ) = X0
+X( : , 1 ) = X0;
 err= 1E6; %intial error assumtion for looping
 while sum(abs(err) >= tol) ~= zeros(na,1)
-    X ( : ,k+ 1 ) = (D+w*L) \ ( D*(1-w) - w*U)*X( : ,k) + inv(D+ w*L)*B;% SOR formula
+    X ( : ,k+ 1 ) = (D+w*L) \ ( D*(1-w) - w*U)*X( : ,k) + (D+ w*L)\B;% SOR formula
     err = X( :,k+1) - X( :, k);% finding error
-    disp(err)
     k = k + 1; 
 end
  
@@ -140,84 +155,120 @@ fprintf (' The final ans obtaibed after %d itaration which is  \n', k)
 X( : ,k)
 end
 
-
-
-
-
-
-
-
 function [row, rhs] = detBC(cells,id,dx,dy)
     global M N u0 uR totalPts nPnts nMiss;
     rr = find((nPnts-id)>=0,1);
     if(rr>1)
-        cc = id-nPnts(rr-1);
-    else
-        cc = id;
+        cc = 1+id-nPnts(rr-1);
     end
     row = zeros(1,totalPts);
-    grid = cells(rr:rr+1,cc:cc+1);
-    
-    if(sum(grid(1,:)+grid(2,:))==4 ||sum(grid(1,:)+grid(2,:))==3)%if an interior point no BCS just laplace
-        row(id-1:id+1)=[1.0, -4.0, 1.0]/(dx*dx);
-        if(rr>=2)
-            row(id-(M-nMiss(rr-1)))=1.0/(dy*dy);%top point
-            row(nPnts(rr)+cc)=1.0/(dy*dy);%bottom point
-        %else
-        %    row(cc)=1.0;
-        %    row(nPnts(rr)+cc)=1.0;
-        end
+    %determine if on a boundary
+    bcv=0;%boundary condition flags
+    bch=0;
+    if(cc==2)
+        bch=-1;
+    elseif(cc==M-1)
+        bch=1;
+    end
+    if(rr==2)
+        bcv=1;
+    elseif(rr==N-1)
+        bcv=-1;
+    end
+    %check BC's
+    %check for no Boundary
+    if(abs(bch)+abs(bcv)==0)
+       row(id-1:id+1)=[1.0,-4.0,1.0];
+       row(id-(M-2-nMiss(rr-1)))=1.0;%top point
+       row(nPnts(rr)+cc-1)=1.0;%bottom point
+       rhs=0.0;
+       return
+    end
+    %check horizontal
+    if(bch==-1)%left side through
+        row(id:id+1)=row(id:id+1)+[-1.0,1.0];
+        rhs = u0*dx;
+    elseif(bch==1)%right side through
+        row(id-1:id)=row(id-1:id)+[1.0,-1.0];
+        rhs = -u0*dx;
+    elseif(bch==0)
+        row(id-1:id+1)=[1.0,-2.0,1.0];
         rhs=0.0;
-    else
-        if(sum(abs(grid(:,1)))==0)%left solid bc
-            %none yet algorithm can't catch this yet
+    end
+    %check vertical
+    if(bcv==-1)%solid bottom
+        row(id)=row(id)-1.0;
+        row(id-(M-2-nMiss(rr-1)))=row(id-(M-2-nMiss(rr-1)))+1.0;
+    elseif(bcv==1)%top solid
+        row(id) = row(id)-1.0;
+        row(nPnts(rr)+cc-1)=row(nPnts(rr)+cc-1)+1.0;
+    elseif(bcv==0)
+        row(id-(M-2-nMiss(rr-1)))=1.0;%top point
+        row(nPnts(rr)+cc-1)=1.0;%bottom point
+        row(id)=row(id)-2.0;
+    end
+
+end
+
+function solution = rebuildGrid(sol,nPnts)
+    global M N;
+    solution = zeros(M-2,N-2);
+    id = 1;
+    for i=2:M-1
+        for j =1:(nPnts(i)-nPnts(i-1))
+            solution(i-1,j)=sol(id);
+            id=id+1;
         end
-        if(sum(abs(grid(:,2)))==0)%right solid bc
-            row(id-1:id)=row(id-1:id)+[-1.0 1.0]/dx;
-            row(id-2:id)=row(id-2:id)+[1.0 -2.0 1.0]/(dx*dx);  %backward laplace
+    end
+            
+end
+
+function [velx,vely] = detVel(sol,dx)
+    %REQUIRES CHANGE FOR GEOMETRY
+    global M N;
+    dim = size(sol);
+    velx = zeros(size(sol));
+    vely = zeros(size(sol));
+    for i=1:dim(1)
+        for j=1:dim(2)
+            comy=0;
+            comx=0;
+            if(i==1)%top boundary
+                vely(i,j)=(sol(i,j)-sol(i+1,j))/dx;
+                comy=comy+1;
+            end
+            if(i==M-2)
+                vely(i,j)=(sol(i-1,j)-sol(i,j))/dx;
+                comy=comy+1;
+            end
+            if(j==1)
+                velx(i,j)=(sol(i,j+1)-sol(i,j))/dx;
+                comx=comx+1;
+            end
+            if(j==N-2)
+                velx(i,j)=(sol(i,j)-sol(i,j-1))/dx;
+                comx=comx+1;
+            end
+            if(~comx)
+                velx(i,j)=(sol(i,j+1)-sol(i,j-1))/(2.0*dx);
+            end
+            if(~comy)
+                vely(i,j)=(sol(i+1,j)-sol(i-1,j))/(2.0*dx);
+            end
             
         end
-        if(sum(abs(grid(1,:)))==0)%top solid bc
-            row(id) = row(id)+1.0/dy;
-            row(nPnts(rr)+cc)=row(nPnts(rr)+cc)-1.0/dy;%these might need sign change (depend on nhat)
-            %backward Laplace (y)
-            row(id)=row(id)+1.0/(dy*dy);
-            row(nPnts(rr)+cc)=row(nPnts(rr)+cc)-2.0/(dy*dy);
-            row(nPnts(rr+1)+cc)=row(nPnts(rr)+cc)+1.0/(dy*dy);      
-        end
-        if(sum(abs(grid(2,:)))==0)%bottom solid bc
-            row(id)=row(id)-1.0/dy;
-            row(id-(M-nMiss(rr-1)))=row(id-(M-nMiss(rr-1)))+1.0/dy;
-            %forward Laplace (y)
-            row(id)=row(id)+1.0/(dy*dy);%center point
-            row(id-(M-nMiss(rr-1)))=row(id-(M-nMiss(rr-1)))-2.0/(dy*dy);
-            row(id-(2*M-nMiss(rr-1)-nMiss(rr-2)))=row(id-(2*M-nMiss(rr-1)-nMiss(rr-2)))+1.0/(dy*dy);
-        end
-        if(sum(grid(:,2))<=0)%right through bc
-           row(id-1:id)=row(id-1:id)+[-1.0 1.0]/dx; 
-           row(id-2:id)=row(id-2:id)+[1.0 -2.0 1.0]/(dx*dx);  %backward laplace
-           rhs=uR;
-        end
-        if(sum(grid(:,1))<=0)%left through bc
-           row(id:id+1)=row(id:id+1)-[-1.0 1.0]/dx;%might need sign change
-           row(id:id+2)=row(id:id+2)+[1.0 -2.0 1.0]/(dx*dx);  %backward laplace
-           rhs=u0;%rhs needed
-        end  
-        %%Add forward backward or centered laplace to cells x or y laplace
-        if((sum(grid(:,1))==1 || sum(grid(:,1))==2)&& (sum(grid(:,2))==1 || sum(grid(:,2))==2))%no left or right BC
-            row(id-1:id+1)=row(id-1:id+1)+[1.0, -2.0, 1.0]/(dx*dx);
-            rhs=0.0;
-        end
-        if((sum(grid(1,:))==1 || sum(grid(1,:))==2)&& (sum(grid(2,:))==1 || sum(grid(2,:))==2))%no top or bottom BC (corner)
-            row(id)=row(id)-2.0/(dy*dy);%center point
-            row(id-(M-nMiss(rr-1)))=row(id-(M-nMiss(rr-1)))+1.0/(dy*dy);
-            row(nPnts(rr)+cc)=row(nPnts(rr)+cc)-1.0/(dy*dy);
-            rhs=0.0;
-        end
-        
     end
 end
 
+function drawCells(domX,domY,dx,cells)
+global N;
+cgX=linspace(domX(1)-dx,domX(2)+dx,N+2);
+cgY=linspace(domY(1)-dx,domY(2)+dx,N+2);
+[cx,cy]=meshgrid(cgX,cgY);
+C = [[cells zeros(size(cells,1),1)] ; zeros(1,size(cells,2)+1)];
+colormap('colorcube');
+pcolor(cx,cy,C);
+end
 
 
 

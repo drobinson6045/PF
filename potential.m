9b0d126de55d4201d785eab7ee7ea700e55be11a
drobinson6045 <drobinson6045@live.com>
2019-03-07 13:31:45 -0500

    N=M only

--- potential.m
+++ potential.m
@@ -1,12 +1,12 @@
-global M N u0 uR totalPts nPnts nMiss;
 
 
+clear();
 
 
-M = 37;
+M = 12;
 
-N = 32;
-u0 = 1.0;
+N = 12;
+u0 = 25.0;
 domX = [0 1.0];
 domY = [0 1.0];
 dx = (domX(2)-domX(1))/(N-1);
@@ -21,58 +21,60 @@ cells(M+1,:)=0.0;
 
 
 %Initialization
-nSolid = 0;%this is a bit sloppy (Has to match with filling of solid cells
-    %Fill solid cells for outflow boundary
-%cells(M-1:M,N-1:N)=0.0;
 
-totalPts = (M-2)*(N-2)-nSolid;
-rhs = zeros(totalPts);
-A = zeros(totalPts,totalPts);
-b = zeros(totalPts,1);
-x0 = zeros(totalPts,1);
+    %Fill solid cells for outflow boundary
+cells(M-3:M,N-1:N)=0.0;
+cells(M-3:M,1:3)=0.0;
+
 nPnts = zeros(M,1);
 nMiss = zeros(M,1);
 
 %fill solid cells to block outflow
 for j = 1:M+1
-    if(cells(j,N)==0)
+    if(cells(j,N)==0)%right side
         cells(j,N+1)=0.0;
+    end
+    if(cells(j,2)==0)%left side
+        cells(j,1)=0.0;
     end
 end
 
 
-    %account for non square geometry
+    %build mapping between column and grid space
+idLoc = {};
+idMap = zeros(M,N);
 total=0;
 for j=2:M-1
     count=0;
     for k=2:N-1
-        if(sum(abs(cells(j:j+1,k))+abs(cells(j:j+1,k+1)))==4)
+        cellSum = sum(abs(cells(j:j+1,k))+abs(cells(j:j+1,k+1)));
+        if(cellSum==4 || cellSum==3 )%corner or non boundary point
             total=total+1;
             count=count+1;
+            idLoc=[idLoc, [j k] ];
+            idMap(j,k)=total;
         end
     end
-    nPnts(j) = total;
-    nMiss(j)= M-2-count;
+end
+totalPts = total;
+rhs = zeros(totalPts);
+x0 = zeros(totalPts,1);
+%count missing right through cells
+cLeft=0;
+cRight=0;
+for i=2:M
+    if(cells(i,N+1)==-1)
+        cRight=cRight + 1;
+    end
+    if(cells(i,1)==-1)
+        cLeft=cLeft + 1;
+    end
 end
 
-
-%count missing right cells
-% count=0;
-% for i=1:M
-%     if(nPnts(i)-i*N == 0)
-%         count=count + 1;
-%     end
-% end
-
-uR = u0*(M-1)/(count-1);%conservation motivated
-    
-
+uR = u0*cLeft/cRight;%conservation motivated
+%uR = u0;
 %fill array
-for id=1:totalPts
-    [row, rhs] = detBC(cells,id,dx,dy);
-    A(id,:)=row;
-    b(id)=rhs;
-end
+[A, b] = buildMatrix(cells,dx,dy,u0,uR,totalPts, idLoc, idMap);
 disp("built matrix")
 %build initial guess of uniform flow
 for i=2:M-1
@@ -86,20 +88,22 @@ for i=2:M-1
        end
    end
 end
-
+%x0 = ones(totalPts,1);
 x = sor(A,b,x0,1E-3);
 
-solution = rebuildGrid(x,nPnts);
-[vx,vy] = detVel(solution,dx);
+solution = rebuildGrid(x,M,N,idLoc,totalPts);
+[vx,vy] = detVel(solution,dx,M,N);
 
 %draw cells
-drawCells(domX,domY,dx,cells);
+drawCells(domX,domY,dx,cells,M,N);
 
 hold on
 %draw vel field
-[xm, ym] = meshgrid(xGrid(2:N-1),yGrid(2:M-1));
-quiver(xm,ym,vx,vy);
-
+[xm, ym] = meshgrid(xGrid(2:N-1),flip(yGrid(2:M-1)));
+q=quiver(xm,ym,vx,vy);
+q.LineWidth=1.0;
+axis equal
+streamline(xm,ym,vx,vy);
 
 
 
@@ -128,17 +132,13 @@ if abs(e) >= 1
     return
 end
  
-
 [nx, mx] = size(x0);
 if nx ~= na || mx ~= 1
     disp( 'ERROR: check input')
     return
 end
 
- 
 %allowed error in final answer
- 
-
 tol = t*ones(na,1);
 k= 1;
  
@@ -151,80 +151,154 @@ while sum(abs(err) >= tol) ~= zeros(na,1)
 end
  
 fprintf (' The final ans obtained after %d iterations\n', k)
-x( : ,k)
+x=x( : ,k)
 end
 
-function [row, rhs] = detBC(cells,id,dx,dy)
-    global M N u0 uR totalPts nPnts nMiss;
-    rr = find((nPnts-id)>=0,1);
-    if(rr>1)
-        cc = 1+id-nPnts(rr-1);
-    end
-    row = zeros(1,totalPts);
-    %determine if on a boundary
-    bcv=0;%boundary condition flags
-    bch=0;
-    if(cc==2)
-        bch=-1;
-    elseif(cc==N-1)
-        bch=1;
-    end
-    if(rr==2)
-        bcv=1;
-    elseif(rr==M-1)
-        bcv=-1;
-    end
-    %check BC's
-    %check for no Boundary
-    if(abs(bch)+abs(bcv)==0)
-       row(id-1:id+1)=[1.0,-4.0,1.0];
-       row(id-(M-2-nMiss(rr-1)))=1.0;%top point
-       row(nPnts(rr)+cc-1)=1.0;%bottom point
-       rhs=0.0;
-       return
-    end
-    %check horizontal
-    if(bch==-1)%left side through
-        row(id:id+1)=row(id:id+1)+[-1.0,1.0];
-        rhs = u0*dx;
-    elseif(bch==1)%right side through
-        row(id-1:id)=row(id-1:id)+[1.0,-1.0];
-        rhs = -u0*dx;
-    elseif(bch==0)
-        row(id-1:id+1)=[1.0,-2.0,1.0];
+function [A,b] = buildMatrix(cells,dx,dy,u0,uR,totalPts, idLoc, idMap)
+
+    A=zeros(totalPts,totalPts);
+    b = zeros(totalPts,1);
+    for id=1:totalPts
+        loc = idLoc{1,id};
+        row = zeros(1,totalPts);
+        %determine if on a boundary
+        bcv=0;%boundary condition flags
+        bch=0;
         rhs=0.0;
-    end
-    %check vertical
-    if(bcv==-1)%solid bottom
-        row(id)=row(id)-1.0;
-        row(id-(M-2-nMiss(rr-1)))=row(id-(M-2-nMiss(rr-1)))+1.0;
-    elseif(bcv==1)%top solid
-        row(id) = row(id)-1.0;
-        row(nPnts(rr)+cc-1)=row(nPnts(rr)+cc-1)+1.0;
-    elseif(bcv==0)
-        row(id-(M-2-nMiss(rr-1)))=1.0;%top point
-        row(nPnts(rr)+cc-1)=1.0;%bottom point
-        row(id)=row(id)-2.0;
+        %check BC's
+        %check horizontal
+        shifts = [[1 0]; [0 1]; [-1 0]; [0 -1]];
+        if(mod(id,totalPts/10)==0)
+            disp('Filling array: (id/total)')
+            disp(id/totalPts);
+        end
+
+
+        for i=1:4%check if there are BC's for the point
+            shift=loc+shifts(i,:);
+            if(idMap(shift(1),shift(2))==0)
+                if(mod(i,2)==1)
+                    bcv=2-i;
+                else
+                    bch=3-i;
+                end
+            end
+        end
+
+        %check for no Boundary
+        if(abs(bcv)+abs(bch)==0)
+           row(id-1:id+1)=[1.0,-4.0,1.0];
+           row(idMap(loc(1)-1,loc(2)))=1.0;%top point
+           row(idMap(loc(1),loc(2)+1))=1.0;%bottom point
+           rhs=0.0;
+           
+        end
+        %check horizontal
+        if(bch~=0)%if horizontal bc
+            %check if neumann or dirichlet
+            if(bch>0)
+                row(id-1:id)=row(id-1:id)+[1.0,-1.0];
+                cellSum=sum(cells(loc(1):loc(1)+1,loc(2)+2));
+            else
+                row(id:id+1)=row(id:id+1)+[-1.0,1.0];
+                cellSum=sum(cells(loc(1):loc(1)+1,loc(2)-1));
+            end
+            if(cellSum==0)%dirichlet bc
+                rhs=0.0;
+            elseif(cellSum<0)%through bc
+                if(bch<0)
+                    rhs = u0*dx;
+                else
+                    rhs = -uR*dx;
+                end
+            else%corner
+                row(id)=row(id)-1.0;
+                %find other referene point
+                m= [0 bch];
+                curPoint = loc + m;
+
+                %decide on which way to step
+                above = sum(cells(curPoint(1),curPoint(2):curPoint(2)+1));
+                if(above>0 || (above==0 && sum(abs(cells(curPoint(1),curPoint(2):curPoint(2)+1)))~=0))
+                    curPoint(1)=curPoint(1)-1;%move up one
+                else
+                    curPoint(1)=curPoint(1)+1;%move down one
+                end
+                %check if valid
+                if(idMap(curPoint(1),curPoint(2))==0)%at a boundary point
+                    %on boundary need to figure out bc of point
+                    if(bch<0)
+                        side=0;
+                    else
+                        side=1;
+                    end
+                    cellSum = sum(cells(curPoint(1):curPoint(1)+1,curPoint(2)+side));%sum on bc side
+                    curPoint(2)=curPoint(2)-bch;%guaranteed to be this point
+                    if(cellSum==0)%solid boundary
+                        rhs = rhs+0.0;
+                    else %through boundary
+                        if(bch < 0)
+                            rhs=rhs + u0*dx;
+                        else
+                            rhs=rhs-uR*dx;
+                        end
+                    end
+                end   
+                row(idMap(curPoint(1),curPoint(2)))=1.0;%assign value at correspoding column
+            end
+        elseif(bch==0 && bcv~=0)
+            row(id-1:id+1)=row(id-1:id+1)+[1.0, -2.0, 1.0];
+        end%end horizontal bc 
+
+
+    %check vertical  
+        if(bcv~=0)%if vertical bc
+            %take care of points we know
+            if(bcv>0)
+                row(id)=row(id)-1.0;
+                row(idMap(loc(1)-1,loc(2)))=1.0;%top point
+                cellSum=sum(cells(loc(1)+2,loc(2):loc(2)+1));%sum bottom of next point
+                curPoint = loc + [1 0];
+            else
+                row(id)=row(id)-1.0;
+                row(idMap(loc(1)+1,loc(2)))=1.0;%bottom point
+                cellSum=sum(cells(loc(1)-1,loc(2):loc(2)+1));%sum top of next point
+                curPoint = loc + [-1 0];
+            end
+            if(cellSum~=0)%we are at a vertical wall
+                sumLeft=sum(cells(curPoint(1):curPoint(1)+1,curPoint(2)));
+                if(sumLeft==0)%left boundary 
+                    curPoint(2)=curPoint(2)+1;%take right point
+                else
+                    curPoint(2)=curPoint(2)-1;%take left point
+                end
+                row(idMap(curPoint(1),curPoint(2)))=1.0;
+                row(id)=row(id)-1.0;
+            end
+        elseif(bcv==0 && bch~=0)
+            row(id)=row(id)-2.0;
+            row(idMap(loc(1)-1,loc(2)))=1.0;%top point
+            row(idMap(loc(1)+1,loc(2)))=1.0;%bottom point
+        end%end veritical bc 
+        A(id,:)=row;
+        b(id)=rhs;
     end
 
 end
 
-function solution = rebuildGrid(sol,nPnts)
-    global M N;
+function solution = rebuildGrid(sol,M,N,idLoc,totalPts)
+  
     solution = zeros(M-2,N-2);
-    id = 1;
-    for i=2:M-1
-        for j =1:(nPnts(i)-nPnts(i-1))
-            solution(i-1,j)=sol(id);
-            id=id+1;
-        end
+    
+    for i=1:totalPts
+        loc = idLoc{1,i};
+        solution(loc(1)-1,loc(2)-1)=sol(i);
     end
             
 end
 
-function [velx,vely] = detVel(sol,dx)
+function [velx,vely] = detVel(sol,dx,M,N)
     %REQUIRES CHANGE FOR GEOMETRY
-    global M N;
     dim = size(sol);
     velx = zeros(size(sol));
     vely = zeros(size(sol));
@@ -232,37 +306,51 @@ function [velx,vely] = detVel(sol,dx)
         for j=1:dim(2)
             comy=0;
             comx=0;
-            if(i==1)%top boundary
-                vely(i,j)=(sol(i,j)-sol(i+1,j))/dx;
-                comy=comy+1;
-            end
-            if(i==M-2)
-                vely(i,j)=(sol(i-1,j)-sol(i,j))/dx;
-                comy=comy+1;
-            end
-            if(j==1)
-                velx(i,j)=(sol(i,j+1)-sol(i,j))/dx;
-                comx=comx+1;
-            end
-            if(j==N-2)
-                velx(i,j)=(sol(i,j)-sol(i,j-1))/dx;
-                comx=comx+1;
-            end
-            if(~comx)
-                velx(i,j)=(sol(i,j+1)-sol(i,j-1))/(2.0*dx);
-            end
-            if(~comy)
-                vely(i,j)=(sol(i+1,j)-sol(i-1,j))/(2.0*dx);
+            if(sol(i,j)~=0)
+                if(i==1)%top boundary
+                    vely(i,j)=(sol(i,j)-sol(i+1,j))/dx;
+                    comy=comy+1;
+                end
+                if(i==M-2)
+                    vely(i,j)=(sol(i-1,j)-sol(i,j))/dx;
+                    comy=comy+1;
+                end
+                if(j==1)
+                    velx(i,j)=(sol(i,j+1)-sol(i,j))/dx;
+                    comx=comx+1;
+                end
+                if(j==N-2)
+                    velx(i,j)=(sol(i,j)-sol(i,j-1))/dx;
+                    comx=comx+1;
+                end
+                if(~comx)
+                    if(sol(i,j+1)==0)
+                        velx(i,j)=(sol(i,j)-sol(i,j-1))/dx;
+                    elseif(sol(i,j-1)==0)
+                        velx(i,j)=(sol(i,j+1)-sol(i,j))/dx;
+                    else
+                        velx(i,j)=(sol(i,j+1)-sol(i,j-1))/(2.0*dx);
+                    end
+                end
+                if(~comy)
+                    if(sol(i+1,j)==0)
+                        vely(i,j)=(sol(i-1,j)-sol(i,j))/dx;
+                    elseif(sol(i-1,j)==0)
+                        vely(i,j)=(sol(i,j)-sol(i+1,j))/dx;
+                    else
+                        vely(i,j)=(sol(i+1,j)-sol(i-1,j))/(2.0*dx);
+                    end
+                end
             end
             
         end
     end
 end
 
-function drawCells(domX,domY,dx,cells)
-global N M;
+function drawCells(domX,domY,dx,cells,M,N)
+
 cgX=linspace(domX(1)-dx,domX(2)+dx,N+2);
-cgY=linspace(domY(1)-dx,domY(2)+dx,M+2);
+cgY=linspace(domY(2)+dx,domY(1)-dx,M+2);
 [cx,cy]=meshgrid(cgX,cgY);
 C = [[cells zeros(size(cells,1),1)] ; zeros(1,size(cells,2)+1)];
 colormap('colorcube');
